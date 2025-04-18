12bfed0c2bd639599efbe18b0ba7cd6ac1b75df8
Saivinay GitHub <p.saivinay98@gmail.com>
2025-04-17 19:30:50 -0500

    day1

---
+++ day1/go.mod
@@ -0,0 +1,3 @@
+module github.com/Saivinay1464/Go-Learning/day1
+
+go 1.24.2
---
+++ day1/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+const day = 2
+
+func main() {
+	fmt.Printf("Hello, Today is Day %d of my GO journey", day)
+}
